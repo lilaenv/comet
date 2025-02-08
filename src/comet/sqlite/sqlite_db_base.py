75456321aed11e7444a93aef8776aaae5ee4fe9a
lilaenv <192981082+lilaenv@users.noreply.github.com>
2025-02-09 01:27:36 +0900

    feat: db の設定ロジックはここに集約

---
+++ src/comet/sqlite/sqlite_db_base.py
@@ -0,0 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class SQLiteDbBase:
+    """Base class for SQLite database."""
+
+    # 環境変数は通常 _env.py でロードする
+    # sqlite に関するロジックはこのディレクトリに集約する
+    # そのためここで SQLITE_DB_NAME をロードする
+    DB_NAME: str = os.environ["SQLITE_DB_NAME"]
