24f9561abbf97545b4ab5fc0f3a204f04314a972
lilaenv <192981082+lilaenv@users.noreply.github.com>
2025-02-11 16:26:27 +0900

    feat: 利用可能。以下の変更を含みます。
    - user_id を削除
    - insert メソッドの実装

--- src/comet/data/sqlite/moderation_dao.py
+++ src/comet/data/sqlite/moderation_dao.py
@@ -1,6 +1,8 @@
 import aiosqlite
 
-from src.comet.sqlite.sqlite_db_base import SQLiteDbBase
+from src.comet.data.dto.moderation_dto import ModerationDTO
+
+from .sqlite_db_base import SQLiteDbBase
 
 
 class ModerationDAO(SQLiteDbBase):
@@ -15,14 +17,35 @@ class ModerationDAO(SQLiteDbBase):
         try:
             query = f"""
             CREATE TABLE IF NOT EXISTS {self._table_name} (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                user_id INTEGER NOT NULL,
-                moderation_id TEXT NOT NULL,
+                id              INTEGER PRIMARY KEY AUTOINCREMENT,
+                moderation_id   TEXT NOT NULL,
                 category_scores TEXT NOT NULL,
-                flagged BOOLEAN NOT NULL,
+                flagged         BOOLEAN NOT NULL
             );
             """
             await conn.execute(query)
+            await conn.commit()
+        finally:
+            await conn.close()
+
+    async def insert(self, moderation_result: ModerationDTO) -> None:
+        # フラグが True のときのみ記録する
+        if moderation_result.flagged is False:
+            return
+        conn = await aiosqlite.connect(super().DB_NAME)
+        try:
+            query = """
+            INSERT INTO moderation (moderation_id, category_scores, flagged)
+            VALUES (?, ?, ?);
+            """
+            await conn.execute(
+                query,
+                (
+                    moderation_result.moderation_id,
+                    moderation_result.category_scores,
+                    moderation_result.flagged,
+                ),
+            )
             await conn.commit()
         finally:
             await conn.close()
