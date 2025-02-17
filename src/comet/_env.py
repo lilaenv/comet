c2a2b075f41e236af73df2c6220951e8334bb177
lilaenv <192981082+lilaenv@users.noreply.github.com>
2025-02-17 21:11:18 +0900

    fix: typo を修正

--- src/comet/_env.py
+++ src/comet/_env.py
@@ -7,14 +7,15 @@ load_dotenv()
 # ------ Discord Settings ------
 AUTHORIZED_SERVER_IDS: list[int] = list(map(int, os.environ["AUTHORIZED_SERVER_IDS"].split(",")))
 ADMIN_USER_IDS: list[int] = list(map(int, os.environ["ADMIN_USER_IDS"].split(",")))
+BOT_NAME: str = os.environ["BOT_NAME"]
 MAX_CHARS_PER_RESPONSE: int = int(os.environ["MAX_CHARS_PER_RESPONSE"])
 # ------------------------------
 
 # ------ Anthropic Setings ------
-ANTHROPIC_MAX_CONTEXT_WINDOW: int = int(os.environ["ANTHROPIC_MAX_CONTECXT_WINDOW"])
-ANTHROPIC_MAX_TOKENS: int = int(os.environ["ANTHRIPIC_MAX_TOKENS"])
+ANTHROPIC_MAX_CONTEXT_WINDOW: int = int(os.environ["ANTHROPIC_MAX_CONTEXT_WINDOW"])
+ANTHROPIC_MAX_TOKENS: int = int(os.environ["ANTHROPIC_MAX_TOKENS"])
 ANTHROPIC_DEFAULT_TEMPERATURE: float = float(os.environ["ANTHROPIC_DEFAULT_TEMPERATURE"])
-ANTHROPIC_DEFAULT_TOP_P: float = float(os.environ["ANTHRIPIC_DEFAULT_TOP_P"])
+ANTHROPIC_DEFAULT_TOP_P: float = float(os.environ["ANTHROPIC_DEFAULT_TOP_P"])
 # -------------------------------
 
 # ------ OpenAI Common Settings ------
